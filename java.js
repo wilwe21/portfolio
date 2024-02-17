cabcf069cc9bc14932d619531e82360b25a17964
wilwe <wilwe036@gmail.com>
2024-02-17 20:23:11 +0100

    screens to themes

--- java.js
+++ java.js
@@ -3,3 +3,53 @@ const about = () => {
   	<h1>My Aylurs gtk shell config</h1></br>\
   	<div class='line'>I'm making it by my own <div class='lt'>with some stolen code</div> to look and work that i want it to work. In top right corner you have link to GitHub with all code and hyprland config, themes, wallpapers and others</div>"
 }
+const macchiato = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>Catppuccin Macchiato</h1></br>\
+  	<div class='macchiato'></div>"
+}
+const latte = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>Catppuccin Latte</h1></br>\
+  	<div class='latte'></div>"
+}
+const spaghett = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>Flying Spaghetti Monster</h1></br>\
+  	<div class='spaghett'></div>"
+}
+const mlp = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>My Little Pony</h1></br>\
+  	<div class='mlp'></div>"
+}
+const pacman = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>Pacman</h1></br>\
+  	<div class='pacman'></div>"
+}
+const weirdmagedon = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>Weirdmagedon</h1></br>\
+  	<div class='wg'></div>"
+}
+const kgb = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>The Land of Mushrooms</h1></br>\
+  	<div class='kgb'></div>"
+}
+const jp2 = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>John Paul II</h1></br>\
+  	<div class='jp'></div>"
+}
+const fnaf = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>Freaddy FNAF</h1></br>\
+  	<div class='fnaf'></div>"
+}
+const fuck = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>Fucked Up</h1></br>\
+  	<div class='fuck'></div>"
+}
