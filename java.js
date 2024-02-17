ae7e0c84b26927f2927f016ab8b6535d456afed3
wilwe <wilwe036@gmail.com>
2024-02-17 18:41:50 +0100

    Dots page

---
+++ java.js
@@ -0,0 +1,5 @@
+const about = () => {
+  	document.getElementById("con").innerHTML = "\
+  	<h1>My Aylurs gtk shell config</h1></br>\
+  	<div class='line'>I'm making it by my own <div class='lt'>with some stolen code</div> to look and work that i want it to work. In top right corner you have link to GitHub with all code and hyprland config, themes, wallpapers and others</div>"
+}
