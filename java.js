bacfb5f3b2af35e5f13e4663aa7495c2ce692826
wilwe <wilwe036@gmail.com>
2024-02-18 10:21:29 +0100

    update

--- java.js
+++ java.js
@@ -2,67 +2,67 @@ const about = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>My Aylurs gtk shell config</h1></br>\
   	<div class='line'>I'm making it by my own <div class='lt'>with some stolen code</div>. Configured to look and work as i want it to work. In top right corner you have link to GitHub with all code and hyprland config, themes, wallpapers and others</div>"
-}
+};
 const macchiato = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>Catppuccin Macchiato</h1></br>\
   	<div class='macchiato'></div>\
   	<h3>Main Theme that i think look the best<h3>"
-}
+};
 const latte = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>Catppuccin Latte</h1></br>\
   	<div class='latte'></div>\
   	<h3>Light theme that use a deer-fox wallpaper</h3>"
-}
+};
 const spaghett = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>Flying Spaghetti Monster</h1></br>\
   	<div class='spaghett'></div>\
   	<h3>Best religion ever</h3>"
-}
+};
 const mlp = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>My Little Pony</h1></br>\
   	<div class='mlp'></div>\
   	<h3>Pony in a jar</h3>"
-}
+};
 const pacman = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>Pacman</h1></br>\
   	<div class='pacman'></div>\
   	<h3>Pacman in space. Windows borders look like a pacman maze</h3>"
-}
+};
 const weirdmagedon = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>Weirdmagedon</h1></br>\
   	<div class='wg'></div>\
   	<h3>Theme inspired by Gravity Falls weirdmagedon</h3>"
-}
+};
 const kgb = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>The Land of Mushrooms</h1></br>\
   	<div class='kgb'></div>\
   	<h3>Awesome look to a mushroom land. the moon show actual moon phase</h3>"
-}
+};
 const jp2 = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>John Paul II</h1></br>\
   	<div class='jp'></div>\
   	<h3>O panie to ty na mnie spojrzałeś</h3>"
-}
+};
 const fnaf = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>Freaddy FNAF</h1></br>\
   	<div class='fnaf'></div>\
   	<h3>Freddy FNAF. Freddy nose work as intended</h3>"
-}
+};
 const fuck = () => {
   	document.getElementById("con").innerHTML = "\
   	<h1>Fucked Up</h1></br>\
   	<div class='fuck'></div>\
   	<h3>My name is UI designer</h3>"
-}
+};
 const widgets = () => {
 	document.getElementById('con').innerHTML = "\
 	<div class='center'>\
@@ -80,4 +80,61 @@ const widgets = () => {
 	<h3>Media indicator</h3>\
 	<div class='media'></div>\
 	</div>"
-}
+};
+const shorts = () => {
+	document.getElementById('con').innerHTML ="\
+	<h1>Shortcuts</h1>\
+	<b>Super + Q</b> = run Terminal</br>\
+	<b>Super + X</b> = run Audio Visualizer</br>\
+	<b>Super + C</b> = close current focused app</br>\
+	<b>Alt + F4</b> = close current focused app</br>\
+	<b>Super + E</b> = run File Explorer</br>\
+	<b>Super + v</b> = Toggle Floating</br>\
+	<b>Super + F</b> = Fullscreen</br>\
+	<b>Super + S</b> = open Applauncher</br>\
+	<b>Super + A</b> = open Dashboard</br>\
+	<b>Super + Tab</b> = show Workspaces</br>\
+	<b>Super + Escape</b> = show Powermenu</br>\
+	<b>Super + Shift + Q</b> = show Music Widget</br>\
+	<b>Super + W</b> = show Quick Settings</br>\
+	<b>Super + Shift + Z</b> = show Media Indicator</br>\
+	<b>Super + Shift + Escape</b> = Lockscreen</br>\
+	<b>Super + P</b> = Pseudo tile window</br>\
+	<b>Super + J</b> = Toggle Split</br>\
+	<b>Super + L</b> = run Web Browser on default profile</br>\
+	<b>Super + I</b> = run Web Browser in privet mode</br>\
+	<b>Super + Y</b> = run Web Browser on second profile</br>\
+	<b>Super + Shift + S</b> = Screenshot tool</br>\
+	<b>Super + F1</b> = Mute audio</br>\
+	<b>Super + F2</b> = Decrese volume by 5%</br>\
+	<b>Super + F3</b> = Increse volume by 5%</br>\
+	<b>Super + F12</b> = run calculator</br>\
+	<b>Super + F5</b> = Decrese brightness by 5</br>\
+	<b>Super + F6</b> = Increse brightness by 5</br>\
+	<b>Super + Z</b> = Toggle AGS</br>\
+	<b>Super + P</b> = Pin floating window</br>\
+	<b>Super + Alt + S</b> = Turn of text to speech</br>\
+	<b>Super + G</b> = run Desktop GitHub app</br>\
+	<b>Home</b> = Pause actual playing video player</br>\
+	<b>Super + Shift + F1</b> = Pause actual playing video player</br>\
+	<b>Super + Space</b> = Pause actual playing video player</br>\
+	<b>Super + T</b> = run Text editor</br>\
+	<b>Super + R</b> = run Colorpicker</br>\
+	<b>Super + D</b> = run  System monitor</br>\
+	<b>Super + F4</b> = Toggle Microphone</br>\
+	<b>Super + Left</b> = Move focus to left window</br>\
+	<b>Super + Right</b> = Move Focus to right window</br>\
+	<b>Super + Up</b> = Move focus to up window</br>\
+	<b>Super + Down</b> = Move Focus to down window</br>\
+	<b>Super + Shift + Left</b> = Resize window by 20 to left</br>\
+	<b>Super + Shift + Right</b> = Resize window by 20 to right</br>\
+	<b>Super + Shift + Up</b> = Resize window by 20 to up</br>\
+	<b>Super + Shift + Down</b> = Resize window by 20 to down</br>\
+	<b>Super + 0-9</b> = Switch workspace</br>\
+	<b>Super + Shift + 0-9</b> = Move window to workspace</br>\
+	<b>Super + Mouse Up</b> = Switch to workspace 1 higher</br>\
+	<b>Super + Mouse Down</b> = Switch to workspace 1 lower</br>\
+	<b>Super + Left Click</b> = Move Window</br>\
+	<b>Super + Right Click</b> = Resize Window</br>\
+	<b>Super + Shift + Right Click</b> = Resize Window keep floting window aspectratio"
+};
