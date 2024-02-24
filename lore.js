8db5134c0939ca8284762bdcb589b24554c05e76
wilwe <wilwe036@gmail.com>
2024-02-24 19:05:33 +0100

    si

---
+++ lore.js
@@ -0,0 +1,6 @@
+const begin = () => {
+	document.getElementById('name').innerHTML = "Lore"
+  	document.getElementById("con").innerHTML = "\
+  	<h1>The Story of a little Lamb</h1>\
+  	<h3>Once upona time on a planet milions light years from earth theres a planet called „Ovisa” lives a little lamb. That lamb was very good at learning thins and he was the chosen one who is needed to bring peace in the entire multiverse. The lamb invent a multi dimensional portal device, and he thinking he was ready to discover new dimensions but no one ever was been.</h3>"
+};
