fd83d8de49fa4c530db286a69268d2e6bb3b9b26
wilwe <wilwe036@gmail.com>
2024-06-02 17:24:01 +0000

    Update main.js

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import TopBar from './mainmods/topbar.js'
+import { Link } from 'react-router-dom';
 
 function Main() {
 	return (
 		<div>
 			<TopBar />
-			<link to="/duo" class="Button">chuj</link>
+			<Link to="/duo" class="Button">chuj</Link>
 		</div>
 	);
 }
