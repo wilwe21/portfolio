ea546c59376907aafd509c69548dd053bcd0ed02
wilwe <wilwe036@gmail.com>
2024-06-02 17:18:09 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-	<BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+	<BrowserRouter basename="/">
 		<Routes>
 			<Route path="/main" element={<Main />} />
 			<Route path="/" element={<Duo />} />
