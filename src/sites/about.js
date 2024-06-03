190d8ddaf130f7558699ec15b351f2d02292a808
wilwe <wilwe036@gmail.com>
2024-06-03 20:59:41 +0200

    I'm really excited about burger

--- src/sites/about.js
+++ src/sites/about.js
@@ -1,11 +1,13 @@
-import TopBar from "../modules/topbar.js"
+import TopBar from "../modules/topbar.js";
+import { Link } from "react-router-dom";
 function About() {
 	return (
 		<div>
 			<TopBar left="✨" middle="About Me" right="✨"/>
 			<div class="txt">
-				So
+				So... You want to known more about me. I'm poland programer music creator and 3D artist on my daily drive i use arch (btw). On this site you can finde stuff made by me and some other things that i'm want to share with other people like my social preferences and inspirations.
 			</div>
+			<Link to="/" class="Button">Back</Link>
 		</div>
 	);
 }
