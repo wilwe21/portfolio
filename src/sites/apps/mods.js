c99c1203802cdf78d36117e6011fefb0ba40e33a
wilwe <wilwe036@gmail.com>
2024-06-10 20:02:08 +0200

    mods part2

--- src/sites/apps/mods.js
+++ src/sites/apps/mods.js
@@ -1,10 +1,27 @@
 import { Link } from 'react-router-dom'
 import TopBar from '../../modules/topbar.js'
+import template from '../../templates.js'
+import langs from '../../lang.js'
+
 function Mods() {
 	return (
 		<div>
-			<TopBar middle="Chuj" />
-			<Link to="/apps" class="Button">Back</Link>
+			<TopBar middle="Mods" />
+			<div>
+				<h2>Duo presest</h2>
+				<div class="ModArea">
+					<textarea value={JSON.stringify(template.braill).replace(/,/g, ",\n")} class="Mod" height="300" />
+				</div>
+			</div>
+			<div>
+				<h2>Language</h2>
+				<div class="ModArea">
+					<textarea value={JSON.stringify(langs.braill).replace(/,/g, ",\n")} class="Mod" />
+				</div>
+			</div>
+			<div class="MainBox">
+				<Link to="/apps" class="Button">Back</Link>
+			</div>
 		</div>
 	)
 }
