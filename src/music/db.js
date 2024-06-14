b0c587de187cda2f004eb1288776bad161f5dbbb
wilwe <wilwe036@gmail.com>
2024-06-14 12:56:13 +0200

    more music or smth

--- src/music/db.js
+++ src/music/db.js
@@ -1,6 +1,9 @@
 import MegaLowania from "./sound/sans.mp3";
-import bMp3 from "./sound/b.mp3";
 import MegaLowaniaWave from "./vital/sans.vital";
+import bMp3 from "./sound/b.mp3";
+import quest from "./sound/quest.mp3";
+import lulaby from "./sound/lulaby for dragon.mp3";
+
 const musicDB = {
 	short: [
 		{
@@ -9,8 +12,16 @@ const musicDB = {
 			addown: MegaLowaniaWave,
 		},
 		{
+			name: "???",
+			sound: quest,
+		},
+		{
 			name: "B",
 			sound: bMp3,
+		},
+		{
+			name: "Lulaby for Dragon",
+			sound: lulaby,
 		}
 	],
 	medium: {},
