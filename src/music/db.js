ed146b71a09c5c4dde2312f75527c25ed0d50a1f
wilwe <wilwe036@gmail.com>
2024-06-18 10:39:48 +0200

    ballad

--- src/music/db.js
+++ src/music/db.js
@@ -4,6 +4,7 @@ import bMp3 from "./sound/b.mp3";
 import quest from "./sound/quest.mp3";
 import lulaby from "./sound/lulaby for dragon.mp3";
 import trumpet from "./sound/i broke my trumpet.mp3";
+import ballad from "./sound/ballad from horses.mp3";
 
 const musicDB = {
 	short: [
@@ -29,6 +30,10 @@ const musicDB = {
 		{
 			name: "I broke my trumpet",
 			sound: trumpet,
+		},
+		{
+			name: "Ballad from horses that want to kill you",
+			sound: ballad,
 		}
 	],
 	long: [],
