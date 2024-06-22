57eaf84c940c069679a411de28d0f4457705ebe8
wilwe <wilwe036@gmail.com>
2024-06-22 19:26:00 +0200

    dylan

--- src/music/db.js
+++ src/music/db.js
@@ -5,6 +5,7 @@ import quest from "./sound/quest.mp3";
 import lulaby from "./sound/lulaby for dragon.mp3";
 import trumpet from "./sound/i broke my trumpet.mp3";
 import ballad from "./sound/ballad from horses.mp3";
+import GambleDylan from "./sound/gambledylan.mp3";
 
 const musicDB = {
 	short: [
@@ -34,7 +35,12 @@ const musicDB = {
 		{
 			name: "Ballad from horses that want to kill you",
 			sound: ballad,
+		},
+		{
+			name: "Dylan GambleCore",
+			sound: GambleDylan,
 		}
+
 	],
 	long: [],
 }
