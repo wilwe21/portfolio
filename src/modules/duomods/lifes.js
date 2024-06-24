6ed52eafa552c965af3b646ebea67be29c4409cc
wilwe <wilwe036@gmail.com>
2024-06-24 14:48:38 +0200

    lifes git work properly

--- src/modules/duomods/lifes.js
+++ src/modules/duomods/lifes.js
@@ -3,7 +3,9 @@ function life( userData, setUserData, amount ) {
 	let liv = userData.lives
 	const date = new Date()
 	const milfs = 30 * 60 * 1000
-	const ldat = userData.ldat ? new Date(userData.ldat) : date
+	let ldat = userData.ldat ? new Date(userData.ldat) : date
+	const ldatmmm = ldat.toISOString().slice(0,10).replace(/-/g,"")
+	const datemmm = date.toISOString().slice(0,10).replace(/-/g,"")
 	const max = userData.maxlives
 	const set = () => {
 		let date2 = null
@@ -19,14 +21,20 @@ function life( userData, setUserData, amount ) {
 		return
 	}
 	if (ldat === null) {
-		console.log("null")
+		return
 	}
-	if (amount !== 0 && amount !== undefined) {
-		liv += amount
+	if (ldatmmm < datemmm) {
+		liv = max
+		console.log("max i chuj")
 		set()
+		return
 	}
 	if (ldat <= date) {
 		liv += 1
+		set()
+	}
+	if (amount !== 0 && amount !== undefined) {
+		liv += amount
 		set()
 	}
 }
