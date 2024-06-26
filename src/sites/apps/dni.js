daffbdb99ae4e4a6811c1082b6675719d0c63a54
wilwe <wilwe036@gmail.com>
2024-06-26 09:12:06 +0200

    dni grindset

--- src/sites/apps/dni.js
+++ src/sites/apps/dni.js
@@ -1,12 +1,37 @@
+import { useState, useEffect } from "react"
 import TopBar from "../../modules/topbar.js"
 import Back from '../../modules/back.js'
 
+const defval = [{
+	"name": "test",
+	"day": 2
+},
+{
+	"name": "test2",
+	"day": 4
+}]
+
 function Dni() {
+	const [dni, setDni] = useState(() => {
+		const data = JSON.parse(localStorage.getItem('Dni'))
+		return data ? data : defval
+	})
+	const [popUp, setPopUp] = useState(false)
+	const add = () => {
+		setPopUp(true)
+		console.log(popUp)
+	}
 	return (
 		<div>
-			<TopBar left={<Back />} middle="Chuj" />
-			<div>
-			</div>
+			{popUp && <div>
+				popUp
+			</div>}
+			<TopBar left={Back("/apps")} middle="Chuj" right={<div class="BackButton" onClick={add}>Add</div>} />
+			{dni.map(key => (
+				<>
+				<div>{key.name} {key.day}</div>
+				</>
+			))}
 		</div>
 	)
 }
