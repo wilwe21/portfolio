e9b4994f50e565a0c548d527fc70800c2043adc4
wilwe <wilwe036@gmail.com>
2024-07-04 21:15:56 +0200

    state

--- src/sites/things.js
+++ src/sites/things.js
@@ -6,6 +6,7 @@ function Things() {
 	return (
 		<div>
 			<TopBar left={Back()} middle="This Site as video pls enjoy" right=""/>
+			<h1>State from 20:00 4.7.2024</h1>
 			<video width="256" height="256" controls>
 				<source src={site} type="video/mp4" />
 			</video>
