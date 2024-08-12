82b5d197808550b2610268f1a325082890827084
wilwe <wilwe036@gmail.com>
2024-08-12 18:48:22 +0200

    markdown test

--- src/sites/diary.js
+++ src/sites/diary.js
@@ -1,10 +1,19 @@
 import TopBar from "../modules/topbar.js"
 import Back from "../modules/back.js"
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+const mark = `
+- [X] mark
+- [ ] down
+- [ ] test
+`
 
 function Diary() {
 	return (
 		<div>
 			<TopBar left={Back()} middle="Diary" right="" />
+			<Markdown remarkPlugins={[remarkGfm]}>{mark}</Markdown>
 		</div>
 	)
 }
